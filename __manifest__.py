be0a37f60f645c1657f0bfff26b73aa5fe9a30ba
Matteo Majewski <matteomaj265@gmail.com>
2025-09-03 09:14:35 +0200

    [UPD] Imported FIles

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Agenzia Immobiliare',
+    "depends": [
+        'mail',
+        'base',
+    ],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+
+        "views/estate_property_view.xml",
+        "views/estate_menu_view.xml",
+    ],
+}
---
+++ models/estate_property.py
@@ -0,0 +1,24 @@
+from odoo import api, fields, models
+
+class EstateProprety(models.Model):
+    _name = 'estate.property'
+    _description = 'Agenzia Immobiliare'
+    _inherit = ['mail.thread']
+
+    today = fields.Date.today()
+
+    name = fields.Char(string="Nome Immobile", required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(default=fields.Date.add(today, months=3))
+    property_type = fields.Selection([])
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection([('nord', 'Nord'), ('sud', 'Sud'), ('est', 'Est'), ('ovest', 'Ovest')])
+    active = fields.Boolean()
